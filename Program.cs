9ce84d7d7f51a697a099915c94d5332c9b2c1d07
codeconscious <jon@j28.tech>
2021-02-13 13:54:05 +0900

    Add data-printing class; create file data record

--- Program.cs
+++ Program.cs
@@ -16,23 +16,57 @@ namespace Music
                 }
 
                 var taggedFile = TagLib.File.Create(filename);
-                string title = taggedFile.Tag.Title;
-                TimeSpan duration = taggedFile.Properties.Duration;
-                Console.WriteLine();
-                Console.WriteLine($"  Title: {title}");
-                Console.WriteLine($"  Artist(s): {string.Join(", ", taggedFile.Tag.Performers)}");
-                Console.WriteLine($"  Duration: {duration:c}");
-                Console.WriteLine($"  Genre: {string.Join(", ", taggedFile.Tag.Genres)}");
-                Console.WriteLine($"  Bitrate: {taggedFile.Properties.AudioBitrate}");
-                Console.WriteLine($"  Sample Rate: {taggedFile.Properties.AudioSampleRate:#,##0}");
-                //Console.WriteLine();
-                Console.WriteLine($"  Composers: {taggedFile.Tag.JoinedComposers}");
+
+                var fileRecord = new FileData(
+                    taggedFile.Tag.Title,
+                    taggedFile.Tag.Performers,
+                    taggedFile.Properties.Duration,
+                    taggedFile.Tag.Genres,
+                    taggedFile.Properties.AudioBitrate,
+                    taggedFile.Properties.AudioSampleRate,
+                    taggedFile.Tag.Composers);
+
+                var printer = new DataPrinter(1, "   • ", 1);
+
+                printer.PrintData(fileRecord);
+            }
+        }
+    }
+
+    public record FileData(
+        string Title, string[] Artists, TimeSpan Duration, string[] Genres,
+        int BitRate, int SampleRate, string[] Composers);    
+
+    public class DataPrinter // TODO: Make an interface to allow for different methods
+    {
+        private byte LinesToPrepend { get; set; }
+        private string LineTextToPrepend { get; set; }
+        private byte LinesToAppend { get; set; }
+
+        public DataPrinter(byte linesToPrepend, string lineTextToPrepend, byte linesToAppend)
+        {
+            LinesToPrepend = linesToPrepend;
+            LineTextToPrepend = lineTextToPrepend;
+            LinesToAppend = linesToAppend;
+        }
+
+        public void PrintData(FileData fileData)
+        {
+            for (var prepend = 0; prepend < LinesToPrepend; prepend++)
                 Console.WriteLine();
 
-                // change title in the file
-                //tfile.Tag.Title = "my new title";
-                //tfile.Save();
-            }
+            Console.WriteLine(LineTextToPrepend + $"Title: {fileData.Title}");
+            Console.WriteLine(LineTextToPrepend + $"Artist(s): {string.Join(", ", fileData.Artists)}");
+            Console.WriteLine(LineTextToPrepend + $"Duration: {fileData.Duration:c}");
+            Console.WriteLine(LineTextToPrepend + $"Genre: {string.Join(", ", fileData.Genres)}");
+            Console.WriteLine(LineTextToPrepend + $"Bitrate: {fileData.BitRate}");
+            Console.WriteLine(LineTextToPrepend + $"Sample Rate: {fileData.SampleRate:#,##0}");
+
+            if (fileData.Composers?.Length > 0)
+                Console.WriteLine(LineTextToPrepend + $"Composers: {string.Join("; ", fileData.Composers)}");
+
+            for (var append = 0; append < LinesToAppend; append++)
+                Console.WriteLine();
         }
     }
 }
