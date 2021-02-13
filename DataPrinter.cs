45c31505468a8dc548a72091f6ff41f951049cf0
codeconscious <jon@j28.tech>
2021-02-13 18:20:53 +0900

    Move output formatting to a new method

--- DataPrinter.cs
+++ DataPrinter.cs
@@ -7,6 +7,8 @@ namespace AudioTagger
         private byte LinesToPrepend { get; set; }
         private string LineTextToPrepend { get; set; }
         private byte LinesToAppend { get; set; }
+        private readonly string _separator = ":"; // TODO: Make into a user setting?
+        const sbyte _minTitleWidth = -13; // TODO: Make into a user setting?
 
         public DataPrinter(byte linesToPrepend, string lineTextToPrepend, byte linesToAppend = 0)
         {
@@ -27,13 +29,13 @@ namespace AudioTagger
 
             // TODO: Make labels multilingual
             // TODO: Add label spaces so that data is aligned
-            Console.WriteLine(LineTextToPrepend + $"Title       : {fileData.Title}");
-            Console.WriteLine(LineTextToPrepend + $"Artist(s)   : {string.Join(", ", fileData.Artists)}");
-            Console.WriteLine(LineTextToPrepend + $"Duration    : {fileData.Duration:m\\:ss}");
-            Console.WriteLine(LineTextToPrepend + $"Genre       : {string.Join(", ", fileData.Genres)}");
-            Console.WriteLine(LineTextToPrepend + $"Bitrate     : {fileData.BitRate}");
-            Console.WriteLine(LineTextToPrepend + $"Sample Rate : {fileData.SampleRate:#,##0}");
-            Console.WriteLine(LineTextToPrepend + $"ReplayGain? : {(fileData.HasReplayGainData ? "Yes" : "No")}");
+            PrintFormattedLine("Title", fileData.Title);
+            PrintFormattedLine("Artist(s)", string.Join(", ", fileData.Artists));
+            PrintFormattedLine("Duration", fileData.Duration.ToString("m\\:ss"));
+            PrintFormattedLine("Genres", string.Join(", ", fileData.Genres));
+            PrintFormattedLine("Bitrate", fileData.BitRate.ToString());
+            PrintFormattedLine("Sample Rate", fileData.SampleRate.ToString("#,##0"));
+            PrintFormattedLine("ReplayGain?", fileData.HasReplayGainData ? "Yes" : "No");
 
             if (fileData.Composers?.Length > 0)
                 Console.WriteLine(LineTextToPrepend + $"Composers: {string.Join("; ", fileData.Composers)}");
@@ -49,6 +51,13 @@ namespace AudioTagger
             PrintPrependLines();
             Console.WriteLine("ERROR: " + message);
             PrintAppendLines();            
+        }
+
+        private void PrintFormattedLine(string title, string data)
+        {            
+            Console.Write(LineTextToPrepend);
+            Console.Write(string.Format($"{title,_minTitleWidth}"));
+            Console.WriteLine($"{_separator} {data}");
         }
 
         private void PrintPrependLines()
