b2340b408d27e4d3a08bada6dc5a51dd3d59fc92
codeconscious <jon@j28.tech>
2021-02-13 23:02:38 +0900

    Partial refactor to incorporate updates

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace AudioTagger
 {
@@ -11,39 +14,66 @@ namespace AudioTagger
 
             var printer = new DataPrinter(1, "   • ");
 
-            for (var i = 0; i < args.Length; i++)
+            var argss = new Queue<string>(args.Select(a => a.Trim()));
+
+            Mode mode;
+            if (argss.Peek() == "-u")
             {
-                var filename = args[i]?.Trim();
-
-                if (string.IsNullOrWhiteSpace(filename))
-                {
-                    printer.PrintError("No filename was entered.");
-                    continue;
-                }
-
-                if (!File.Exists(filename))
-                {
-                    printer.PrintError($"File \"{filename}\" was not found.");
-                    continue;
-                }
-
-                var taggedFile = TagLib.File.Create(filename);
-
-                var fileRecord = new FileData(
-                    Path.GetFileName(filename),
-                    taggedFile.Tag.Title,
-                    taggedFile.Tag.Performers,
-                    taggedFile.Properties.Duration,
-                    taggedFile.Tag.Genres,
-                    taggedFile.Properties.AudioBitrate, // TODO: Figure out why this is always 0
-                    taggedFile.Properties.AudioSampleRate,
-                    taggedFile.Tag.Composers,
-                    taggedFile.Tag.ReplayGainTrackGain > 0 || taggedFile.Tag.ReplayGainAlbumGain > 0);
-
-                printer.PrintData(fileRecord);                
+                mode = Mode.Update;
+                argss.Dequeue();
             }
+            else
+            {
+                mode = Mode.Read;
+            }
+
+            var filesData = new List<FileData>();
+            foreach (var filename in argss)
+                filesData.Add(Parser.GetFileRecordOrNull(printer, filename));
+
+            if (mode == Mode.Read)
+                foreach (var fileData in filesData)
+                    if (fileData == null)
+                        printer.PrintError("Skipped file.");
+                    else
+                        printer.PrintData(fileData);
+            else
+                throw new NotImplementedException("Not done yet!");
 
             Console.WriteLine();
         }
     }
+
+    public static class Parser
+    {
+        public static FileData GetFileRecordOrNull(DataPrinter printer, string filename)
+        {
+            if (string.IsNullOrWhiteSpace(filename))
+            {
+                printer.PrintError("No filename was entered.");
+                return null;
+            }
+
+            if (!File.Exists(filename))
+            {
+                printer.PrintError($"File \"{filename}\" was not found.");
+                return null;
+            }
+
+            var taggedFile = TagLib.File.Create(filename);
+
+            return new FileData(
+                Path.GetFileName(filename),
+                taggedFile.Tag.Title,
+                taggedFile.Tag.Performers,
+                taggedFile.Properties.Duration,
+                taggedFile.Tag.Genres,
+                taggedFile.Properties.AudioBitrate, // TODO: Figure out why this is always 0
+                taggedFile.Properties.AudioSampleRate,
+                taggedFile.Tag.Composers,
+                taggedFile.Tag.ReplayGainTrackGain > 0 || taggedFile.Tag.ReplayGainAlbumGain > 0);
+        }
+    }
+
+    public enum Mode { Read, Update }
 }
