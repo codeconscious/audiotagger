2c4d8948b029c47c32e757b540da2c34da3ea720
CodeConscious <hello@codeconscio.us>
2021-02-14 09:32:02 +0900

    Allow null FileData

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ namespace AudioTagger
                 mode = Mode.Read;
             }
 
-            var filesData = new List<FileData>();
+            var filesData = new List<FileData?>();
             foreach (var filename in argss)
                 filesData.Add(Parser.GetFileRecordOrNull(printer, filename));
 
@@ -46,7 +46,7 @@ namespace AudioTagger
 
     public static class Parser
     {
-        public static FileData GetFileRecordOrNull(DataPrinter printer, string filename)
+        public static FileData? GetFileRecordOrNull(DataPrinter printer, string filename)
         {
             if (string.IsNullOrWhiteSpace(filename))
             {
