3db8b02abf5a5ea708e6a48a48873682fe52d91b
codeconscious <hello@codeconscio.us>
2021-03-28 12:30:36 +0900

    Minor layout tweaks

--- FileFilter.cs
+++ FileFilter.cs
@@ -10,7 +10,7 @@ namespace AudioTagger
         public static readonly Func<string, bool> Filter =
             new(file => !string.IsNullOrWhiteSpace(file) &&
                         (file.EndsWith(".mp3", StringComparison) ||
-                        file.EndsWith(".ogg", StringComparison) ||
-                        file.EndsWith(".m4a", StringComparison)));    
+                         file.EndsWith(".ogg", StringComparison) ||
+                         file.EndsWith(".m4a", StringComparison)));
     }
 }
