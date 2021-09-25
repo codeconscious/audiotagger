3c47662a07b3b0ba79de0c6558e8cd412a344bf0
CodeConscious <hello@codeconscio.us>
2021-09-25 21:18:36 +0900

    Add the Spectre.Console library

---
+++ AudioTagger.Console/SpectrePrinter.cs
@@ -0,0 +1,132 @@
+using static System.Console;
+using Spectre.Console;
+
+namespace AudioTagger.Console
+{
+    public class SpectrePrinter : IPrinter
+    {
+        //private static bool _previousWasBlankLine;
+
+        private void PrependLines(byte lines)
+        {
+            if (lines == 0)
+                return;
+
+            for (var prepend = 0; prepend < lines; prepend++)
+                WriteLine();
+        }
+
+        private void AppendLines(byte lines)
+        {
+            if (lines == 0)
+                return;
+
+            for (var append = 0; append < lines; append++)
+                WriteLine();
+        }
+
+        public void Print(string message, byte prependLines = 0, byte appendLines = 0, string prependText = "",
+                                 ConsoleColor? fgColor = null, ConsoleColor? bgColor = null)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+                throw new ArgumentNullException(nameof(message), "Message cannot be empty");
+
+            PrependLines(prependLines);
+
+            var subString = new LineSubString(message, fgColor, bgColor);
+            AnsiConsole.MarkupLine(subString.GetSpecterString());
+
+            AppendLines(appendLines);
+        }
+
+        public void Print(string message, ResultType type, byte prependLines = 0,
+                                 byte appendLines = 0)
+        {
+            Print(message, prependLines, appendLines, GetResultSymbol(type) + " ",
+                  ResultsMap.Map[type].Color, null);
+        }
+
+        public void Print(IReadOnlyList<LineSubString> lineParts, byte prependLines = 0, byte appendLines = 1)
+        {
+            PrependLines(prependLines);
+
+            if (!lineParts.Any())
+                return;
+
+            foreach (var linePart in lineParts)
+            {
+                AnsiConsole.Markup(
+                    new LineSubString(linePart.Text, linePart.FgColor, linePart.BgColor)
+                        .GetSpecterString());
+            }
+
+            AppendLines(appendLines);
+        }
+
+        public void Print(IList<OutputLine> lines, byte prependLines = 0, byte appendLines = 1)
+        {
+            PrependLines(prependLines);
+
+            if (!lines.Any())
+                return; // TODO: Think about this.
+
+            foreach (var line in lines)
+            {
+                var lastLine = line.Line.Last();
+                foreach (var lineParts in line.Line)
+                {
+                    AnsiConsole.Markup(lineParts.GetSpecterString());
+
+                    if (lineParts == lastLine)
+                        WriteLine();
+                }
+            }
+
+            AppendLines(appendLines);
+        }
+
+        public void Error(string message) =>
+            Print(message, 1, 1, "ERROR: ");
+
+        // private void PrintColor(LineSubString lineSubString)
+        // {
+        //     PrintColor(lineSubString.Text, lineSubString.FgColor, lineSubString.BgColor);
+        // }
+
+        // private void PrintColor(string text, ConsoleColor? fgColor,
+        //                                ConsoleColor? bgColor = null,
+        //                                bool addLineBreak = false)
+        // {
+        //     if (fgColor.HasValue)
+        //         System.Console.ForegroundColor = fgColor.Value;
+
+        //     if (bgColor.HasValue)
+        //         BackgroundColor = bgColor.Value;
+
+        //     Write(text);
+
+        //     if (addLineBreak)
+        //         WriteLine();
+
+        //     ResetColor();
+        // }
+
+        // // TODO: Check whether we can delete this.
+        // private void PrintColor()
+        // {
+        //     WriteLine();
+        // }
+
+        public char GetResultSymbol(ResultType type)
+        {
+            return type switch
+            {
+                ResultType.Cancelled => '×',
+                ResultType.Failure => '×',
+                ResultType.Neutral => '-',
+                ResultType.Success => '◯',
+                _ => '?'
+            };
+        }
+    }
+}
--- AudioTagger.Library/LineSubstring.cs
+++ AudioTagger.Library/LineSubstring.cs
@@ -1,19 +1,55 @@
-﻿using System;
-
-namespace AudioTagger
+﻿namespace AudioTagger
 {
-    // Perhaps a good candidate for a record?
     public class LineSubString
     {
         public string Text { get; set; }
-        public ConsoleColor? FgColor { get; set; } = null;
-        public ConsoleColor? BgColor { get; set; } = null;
+        public ConsoleColor? FgColor { get; set; }
+        public ConsoleColor? BgColor { get; set; }
 
         public LineSubString(string text, ConsoleColor? fgColor = null, ConsoleColor? bgColor = null)
         {
             Text = text;
             FgColor = fgColor;
             BgColor = bgColor;
+        }
+
+        public string GetSpecterString()
+        {
+            var sb = new System.Text.StringBuilder();
+
+            if (FgColor.HasValue)
+            {
+                sb.Append("[" + ConvertToSpectreColor(FgColor.Value));
+
+                if (BgColor.HasValue)
+                    sb.Append(" on " + ConvertToSpectreColor(BgColor.Value));
+
+                sb.Append(']');
+            }
+
+            sb.Append(Text.Replace("[", "[[").Replace("]", "]]"));
+
+            if (FgColor.HasValue)
+                sb.Append("[/]");
+
+            // Console.WriteLine("> " + sb.ToString());
+            return sb.ToString();
+
+        }
+
+        private static string ConvertToSpectreColor(ConsoleColor color)
+        {
+            return color switch
+            {
+                ConsoleColor.Red => "red",
+                ConsoleColor.Yellow => "yellow",
+                ConsoleColor.Blue => "blue",
+                ConsoleColor.Green => "green",
+                ConsoleColor.Cyan => "cyan",
+                ConsoleColor.Magenta => "magenta",
+                ConsoleColor.Gray => "gray",
+                _ => "white"
+            };
         }
     }
 }
