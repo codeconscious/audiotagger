73cf8c4e8783be7f3a9483d4a64f7fd427cf9e3d
CodeConscious <hello@codeconscio.us>
2021-11-27 20:49:57 +0900

    Relocate global usings

--- AudioTagger.Console/Program.cs
+++ AudioTagger.Console/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+﻿global using System;
+global using System.Linq;
+global using System.Collections.Generic;
+global using System.Collections.Immutable;
 
 namespace AudioTagger.Console
 {
@@ -8,7 +9,6 @@ namespace AudioTagger.Console
     {
         public static void Main(string[] args)
         {
-            // IPrinter printer = new ConsolePrinter();
             IPrinter printer = new SpectrePrinter();
 
             if (args.Length == 0)
@@ -82,7 +82,6 @@ namespace AudioTagger.Console
             printer.Print("   -v or --view   : View tags");
             printer.Print("   -u or --update : Update tags");
             printer.Print("   -r or --rename : Rename files based on tags (Coming soonish)");
-            // TODO: Add option to disable colors
             // TODO: Make album art opt-in
         }
     }
