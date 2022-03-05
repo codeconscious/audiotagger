650b1352d976c338aa13fcc29d9a3115468e7dd5
CodeConscious <hello@codeconscio.us>
2022-03-05 10:34:47 +0900

    Ignore files whose names begin with a period

--- AudioTagger.Library/FileFilter.cs
+++ AudioTagger.Library/FileFilter.cs
@@ -7,6 +7,7 @@ namespace AudioTagger
         // TODO: Change into a setting of supported file extensions.
         public static readonly Func<string, bool> Filter =
             new(file => !string.IsNullOrWhiteSpace(file) &&
+                        !file.StartsWith(".") &&
                         (file.EndsWith(".mp3", StringComparison.InvariantCultureIgnoreCase) ||
                          file.EndsWith(".ogg", StringComparison.InvariantCultureIgnoreCase) ||
                          file.EndsWith(".m4a", StringComparison.InvariantCultureIgnoreCase)));
