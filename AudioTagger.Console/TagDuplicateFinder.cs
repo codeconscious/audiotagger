85b71b4c95ccec5e09ed9008a5358bbb1a3acda0
CodeConscious <hello@codeconscio.us>
2022-08-06 16:56:30 +0900

    Sort output by artist name; add line numbers; add elapsed time

--- AudioTagger.Console/TagDuplicateFinder.cs
+++ AudioTagger.Console/TagDuplicateFinder.cs
@@ -12,18 +12,32 @@ public class TagDuplicateFinder : IPathOperation
     {
         printer.Print("Checking for duplicates (by artist(s) and title)...");
 
+        var stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+
         var duplicates = mediaFiles
-            .ToLookup(m => string.Concat(m.Artists).Trim() + m.Title.Trim())
+            .ToLookup(m => string.Concat(m.Artists).ToLowerInvariant().Trim() +
+                           m.Title.Trim())
             .Where(m => m.Count() > 1)
             .OrderBy(m => m.Key);
 
-        printer.Print(duplicates.Count() + " found.");
+        var count = duplicates.Count();
+
+        // Using ticks because .ElapsedMilliseconds was wildly inaccurate.
+        // Reference: https://stackoverflow.com/q/5113750/11767771
+        var elapsedMs = TimeSpan.FromTicks(stopwatch.ElapsedTicks).TotalMilliseconds;
+
+        printer.Print($"Found {count} in {elapsedMs:#,##0}ms.");
+
+        uint index = 0;
+        int rightAlign = count.ToString().Length + 2;
 
         foreach (var dupe in duplicates)
         {
+            index++;
             var artist = string.Concat(dupe.First().Artists);
             var title = dupe.First().Title;
-            printer.Print($"  - {artist} /  {title}");
+            printer.Print($"{index.ToString().PadLeft(rightAlign)}. {artist} / {title}");
         }
     }
 }
