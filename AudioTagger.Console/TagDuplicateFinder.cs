977ce2866f2329139f02029b3abf7c37856adc8f
CodeConscious <hello@codeconscio.us>
2022-08-06 18:36:05 +0900

    Improve duplicate-finder output

--- AudioTagger.Console/TagDuplicateFinder.cs
+++ AudioTagger.Console/TagDuplicateFinder.cs
@@ -10,6 +10,12 @@ public class TagDuplicateFinder : IPathOperation
 {
     public void Start(IReadOnlyCollection<MediaFile> mediaFiles, DirectoryInfo workingDirectory, IPrinter printer)
     {
+        if (!mediaFiles.Any())
+        {
+            printer.Print("There are no files to work on. Cancelling...");
+            return;
+        }
+
         printer.Print("Checking for duplicates (by artist(s) and title)...");
 
         var stopwatch = new System.Diagnostics.Stopwatch();
@@ -27,17 +33,17 @@ public class TagDuplicateFinder : IPathOperation
         // Reference: https://stackoverflow.com/q/5113750/11767771
         var elapsedMs = TimeSpan.FromTicks(stopwatch.ElapsedTicks).TotalMilliseconds;
 
-        printer.Print($"Found {count} in {elapsedMs:#,##0}ms.");
+        printer.Print($"Found {count} duplicate{(count == 1 ? "" : "s")} in {elapsedMs:#,##0}ms.");
 
         uint index = 0;
-        int rightAlign = count.ToString().Length + 2;
+        int indexPadding = count.ToString().Length + 2;
 
         foreach (var dupe in duplicates)
         {
             index++;
             var artist = string.Concat(dupe.First().Artists);
             var title = dupe.First().Title;
-            printer.Print($"{index.ToString().PadLeft(rightAlign)}. {artist} / {title}");
+            printer.Print($"{index.ToString().PadLeft(indexPadding)}. {artist} / {title}");
         }
     }
 }
