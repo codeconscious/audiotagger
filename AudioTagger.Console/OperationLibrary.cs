1fe90a22e5b69008b6fcf8ea23bf6007d6e20c2b
CodeConscious <hello@codeconscio.us>
2022-11-20 16:47:19 +0900

    Improve code formatting

--- AudioTagger.Console/OperationLibrary.cs
+++ AudioTagger.Console/OperationLibrary.cs
@@ -8,49 +8,49 @@ public static class OperationLibrary
 
      {
             new(
-                new List<string>
-                {"-v", "--view"},
-                "View tag data", new TagViewer()
+                new List<string>{"-v", "--view"},
+                "View tag data",
+                new TagViewer()
             ),
             new(
-                new List<string>
-                {"-vs", "--view-summary"},
-                "View a tag data summary", new TagSummaryViewer()
+                new List<string>{"-vs", "--view-summary"},
+                "View a tag data summary",
+                new TagSummaryViewer()
             ),
             new(
-                new List<string>
-                {"-u", "--update"},
-                "Update tag data using filenames", new TagUpdater()
+                new List<string>{"-u", "--update"},
+                "Update tag data using filenames",
+                new TagUpdater()
             ),
             new(
-                new List<string>
-                {"-y", "--update-year"},
-                "Update years using Date Created years (Must do before other updates)", new TagUpdaterYearOnly()
+                new List<string>{"-y", "--update-year"},
+                "Update years using Date Created years (Must do before other updates)",
+                new TagUpdaterYearOnly()
             ),
             new(
-                new List<string>
-                {"-r", "--rename"},
-                "Rename and reorganize files into folders based on tag data", new MediaFileRenamer()
+                new List<string>{"-r", "--rename"},
+                "Rename and reorganize files into folders based on tag data",
+                new MediaFileRenamer()
             ),
             new(
-                new List<string>
-                {"-d", "--duplicates"},
-                "List tracks with identical artists and titles (No files are deleted)", new TagDuplicateFinder()
+                new List<string>{"-d", "--duplicates"},
+                "List tracks with identical artists and titles (No files are deleted)",
+                new TagDuplicateFinder()
             ),
             new(
-                new List<string>
-                {"-s", "--stats"},
-                "Display file statistics using tag data", new TagStats()
+                new List<string>{"-s", "--stats"},
+                "Display file statistics using tag data",
+                new TagStats()
             ),
             new(
-                new List<string>
-                {"-m", "--manual"},
-                "Tentatively update specific tags manually using custom code, but without saving the updates", new ManualTagUpdater(saveUpdates: false)
+                new List<string>{"-m", "--manual"},
+                "Test updating specific tags manually using custom code, but without saving the updates",
+                new ManualTagUpdater(saveUpdates: false)
             ),
             new(
-                new List<string>
-                {"-mm", "--mmanual"},
-                "Update specific tags manually using custom code and save the updates", new ManualTagUpdater(saveUpdates: true)),
+                new List<string>{"-mm", "--mmanual"},
+                "Update specific tags manually using custom code and save the updates",
+                new ManualTagUpdater(saveUpdates: true)),
         };
 
         public static string GetHelpText()
