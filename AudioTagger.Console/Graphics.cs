d3974361aae86e7cbd33c67b7c85636cc750289a
CodeConscious <hello@codeconscio.us>
2023-01-03 20:24:20 +0900

    Limit album art graphics to Windows only (#21)
    * Remove unneeded file
    
    * Make graphics-related code Windows only

--- AudioTagger.Console/Graphics.cs
+++ AudioTagger.Console/Graphics.cs
@@ -1,111 +1,111 @@
-using System;
-using System.Linq;
-using System.IO;
+#if _WINDOWS
+
 using System.Drawing;
 using static System.Console;
 
-namespace AudioTagger.Console
+namespace AudioTagger.Console;
+
+public static class Graphics
 {
-    public static class Graphics
+    /// <summary>
+    /// Print an approximation of an image to the console.
+    /// Prints nothing on unsupported platforms.
+    /// </summary>
+    public static void ConsoleWriteImage(byte[] bytes, byte? desiredMaxWidth = null)
     {
-        /// <summary>
-        /// Print an approximation of an image to the console.
-        /// Prints nothing on unsupported platforms.
-        /// </summary>
-        public static void ConsoleWriteImage(byte[] bytes, byte? desiredMaxWidth = null)
-        {
-            var maxWidth = GetValidImageWidth(desiredMaxWidth);
+        var maxWidth = GetValidImageWidth(desiredMaxWidth);
 
-            ConsoleWriteImage(bytes, maxWidth);
+        ConsoleWriteImage(bytes, maxWidth);
+    }
+
+    /// <summary>
+    /// Print an approximation of an image to the console at a specific width.
+    /// Prints nothing on unsupported platforms.
+    /// </summary>
+    /// <param name="bytes">An array of bytes corrresponding to an image.</param>
+    private static void ConsoleWriteImage(byte[] bytes, int maxWidth)
+    {
+        Bitmap bitmap;
+
+        using (var ms = new MemoryStream(bytes))
+        {
+            // TODO: Investigate error csharp(CA1416) here and below.
+            // "This call site is reachable on all platforms. 'Bitmap' is only supported on: 'windows'."
+            bitmap = new Bitmap(ms);
         }
 
-        /// <summary>
-        /// Print an approximation of an image to the console at a specific width.
-        /// Prints nothing on unsupported platforms.
-        /// </summary>
-        /// <param name="bytes">An array of bytes corrresponding to an image.</param>
-        private static void ConsoleWriteImage(byte[] bytes, int maxWidth)
-        {
-            Bitmap bitmap;
+        ConsoleWriteImage(bitmap, maxWidth);
+    }
 
-            using (var ms = new MemoryStream(bytes))
+    /// <summary>
+    /// Print an approximation of an image to the console at a specific width.
+    /// Does nothing on unsupported platforms.
+    /// </summary>
+    /// <see cref="https://www.hanselman.com/blog/how-do-you-use-systemdrawing-in-net-core"/>
+    /// <seealso cref="https://stackoverflow.com/a/33689107/11767771"/>
+    /// <param name="imageBmp"></param>
+    private static void ConsoleWriteImage(Bitmap imageBmp, int maxWidth)
+    {
+        try
+        {
+            var percent = Math.Min(decimal.Divide(maxWidth, imageBmp.Width),
+                                    decimal.Divide(maxWidth, imageBmp.Height));
+
+            var resSize = new Size((int)(imageBmp.Width * percent),
+                                    (int)(imageBmp.Height * percent));
+
+            static int ToConsoleColor(Color c)
             {
-                // TODO: Investigate error csharp(CA1416) here and below.
-                // "This call site is reachable on all platforms. 'Bitmap' is only supported on: 'windows'."
-                bitmap = new Bitmap(ms);
+                int index = (c.R > 128 | c.G > 128 | c.B > 128) ? 8 : 0;
+                index |= (c.R > 64) ? 4 : 0;
+                index |= (c.G > 64) ? 2 : 0;
+                index |= (c.B > 64) ? 1 : 0;
+                return index;
             }
 
-            ConsoleWriteImage(bitmap, maxWidth);
-        }
+            var bmp = new Bitmap(imageBmp, resSize.Width * 2, resSize.Height * 2);
 
-        /// <summary>
-        /// Print an approximation of an image to the console at a specific width.
-        /// Does nothing on unsupported platforms.
-        /// </summary>
-        /// <see cref="https://www.hanselman.com/blog/how-do-you-use-systemdrawing-in-net-core"/>
-        /// <seealso cref="https://stackoverflow.com/a/33689107/11767771"/>
-        /// <param name="imageBmp"></param>
-        private static void ConsoleWriteImage(Bitmap imageBmp, int maxWidth)
-        {
-            try
+            for (int i = 0; i < resSize.Height; i++)
             {
-                var percent = Math.Min(decimal.Divide(maxWidth, imageBmp.Width),
-                                       decimal.Divide(maxWidth, imageBmp.Height));
-
-                var resSize = new Size((int)(imageBmp.Width * percent),
-                                       (int)(imageBmp.Height * percent));
-
-                static int ToConsoleColor(Color c)
+                for (int j = 0; j < resSize.Width; j++)
                 {
-                    int index = (c.R > 128 | c.G > 128 | c.B > 128) ? 8 : 0;
-                    index |= (c.R > 64) ? 4 : 0;
-                    index |= (c.G > 64) ? 2 : 0;
-                    index |= (c.B > 64) ? 1 : 0;
-                    return index;
+                    ForegroundColor = (ConsoleColor)ToConsoleColor(bmp.GetPixel(j * 2, i * 2));
+                    BackgroundColor = (ConsoleColor)ToConsoleColor(bmp.GetPixel(j * 2, i * 2 + 1));
+                    Write("▀");
+
+                    ForegroundColor = (ConsoleColor)ToConsoleColor(bmp.GetPixel(j * 2 + 1, i * 2));
+                    BackgroundColor = (ConsoleColor)ToConsoleColor(bmp.GetPixel(j * 2 + 1, i * 2 + 1));
+                    Write("▀");
                 }
 
-                var bmp = new Bitmap(imageBmp, resSize.Width * 2, resSize.Height * 2);
-
-                for (int i = 0; i < resSize.Height; i++)
-                {
-                    for (int j = 0; j < resSize.Width; j++)
-                    {
-                        ForegroundColor = (ConsoleColor)ToConsoleColor(bmp.GetPixel(j * 2, i * 2));
-                        BackgroundColor = (ConsoleColor)ToConsoleColor(bmp.GetPixel(j * 2, i * 2 + 1));
-                        Write("▀");
-
-                        ForegroundColor = (ConsoleColor)ToConsoleColor(bmp.GetPixel(j * 2 + 1, i * 2));
-                        BackgroundColor = (ConsoleColor)ToConsoleColor(bmp.GetPixel(j * 2 + 1, i * 2 + 1));
-                        Write("▀");
-                    }
-
-                    ResetColor();
-                    WriteLine();
-                }
+                ResetColor();
+                WriteLine();
             }
-            catch
-            {
-                // The platform is unsupported, so do nothing. (Look into libgdiplus or other options.)
-            }
-
-            WriteLine();
         }
-
-        /// <summary>
-        /// Gets a valid maximum size for the width of the image.
-        /// Intended to be used for printing images to the console.
-        /// </summary>
-        /// <param name="desiredMaxSize"></param>
-        /// <returns>A valid image width.</returns>
-        private static int GetValidImageWidth(byte? desiredMaxSize)
+        catch
         {
-            // Division by 2 is needed because images with a width of 60
-            // fit precisely on a console of width 120.
-            var maxPossibleSize = (int)Math.Floor(WindowWidth / 2f);
-
-            return desiredMaxSize == null || desiredMaxSize.Value > maxPossibleSize
-                ? maxPossibleSize
-                : desiredMaxSize.Value;
+            // The platform is unsupported, so do nothing. (Look into libgdiplus or other options.)
         }
+
+        WriteLine();
+    }
+
+    /// <summary>
+    /// Gets a valid maximum size for the width of the image.
+    /// Intended to be used for printing images to the console.
+    /// </summary>
+    /// <param name="desiredMaxSize"></param>
+    /// <returns>A valid image width.</returns>
+    private static int GetValidImageWidth(byte? desiredMaxSize)
+    {
+        // Division by 2 is needed because images with a width of 60
+        // fit precisely on a console of width 120.
+        var maxPossibleSize = (int)Math.Floor(WindowWidth / 2f);
+
+        return desiredMaxSize == null || desiredMaxSize.Value > maxPossibleSize
+            ? maxPossibleSize
+            : desiredMaxSize.Value;
     }
 }
+
+#endif
