e078a7d57e24baf3a18617758fb7bd04ffff1e76
CodeConscious <hello@codeconscio.us>
2023-05-03 22:03:20 +0900

    Add alias for collection

--- AudioTagger.Console/OperationLibrary.cs
+++ AudioTagger.Console/OperationLibrary.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
+using OperationFlags = System.Collections.Generic.List<string>;
 
 namespace AudioTagger.Console;
 
@@ -7,47 +8,47 @@ internal static class OperationLibrary
     internal static readonly IReadOnlyList<Operation> Operations = new List<Operation>()
     {
         new(
-            new List<string>{"-v", "--view"},
+            new OperationFlags{"-v", "--view"},
             "View tag data.",
             new TagViewer()
         ),
         new(
-            new List<string>{"-vs", "--view-summary"},
+            new OperationFlags{"-vs", "--view-summary"},
             "View a tag data summary.",
             new TagViewerSummary()
         ),
         new(
-            new List<string>{"-u", "--update"},
+            new OperationFlags{"-u", "--update"},
             "Update tag data using filename patterns.",
             new TagUpdater()
         ),
         new(
-            new List<string>{"-u1", "--update-single"},
+            new OperationFlags{"-u1", "--update-single"},
             "Update a single tag by manually specifying a value",
             new TagUpdaterSingle()
         ),
         new(
-            new List<string>{"-y", "--update-year"},
+            new OperationFlags{"-y", "--update-year"},
             "Update years using Date Created from file properties. (Must do before other updates.)",
             new TagUpdaterYearOnly()
         ),
         new(
-            new List<string>{"-r", "--rename"},
+            new OperationFlags{"-r", "--rename"},
             "Rename and reorganize files into folders based on tag data.",
             new MediaFileRenamer()
         ),
         new(
-            new List<string>{"-d", "--duplicates"},
+            new OperationFlags{"-d", "--duplicates"},
             "List tracks with identical artists and titles. No files are deleted.",
             new TagDuplicateFinder()
         ),
         new(
-            new List<string>{"-s", "--stats"},
+            new OperationFlags{"-s", "--stats"},
             "Display file statistics using tag data.",
             new TagStats()
         ),
         new(
-            new List<string>{"-n", "--normalize", "--replaygain"},
+            new OperationFlags{"-n", "--normalize", "--replaygain"},
             "Apply track normalization.",
             new Normalization()),
     };
