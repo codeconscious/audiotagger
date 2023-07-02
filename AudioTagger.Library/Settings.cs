824f43350a2f186dd64475c1e8b36b56298e76ba
CodeConscious <hello@codeconscio.us>
2023-07-02 20:30:46 +0900

    Move SettingsService

--- AudioTagger.Library/Settings.cs
+++ AudioTagger.Library/Settings.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace AudioTagger;
@@ -27,4 +29,80 @@ public sealed record Tagging
 {
     [JsonPropertyName("regexPatterns")]
     public ImmutableList<string>? RegexPatterns { get; set; }
+}
+
+public static class SettingsService
+{
+    private const string _settingsFileName = "settings.json";
+
+    public static bool CreateIfMissing(IPrinter printer)
+    {
+        if (File.Exists(_settingsFileName))
+            return true;
+
+        try
+        {
+            return WriteSettingsToFile(new Settings(), printer);
+        }
+        catch (Exception ex)
+        {
+            printer.Error($"There was an error creating \"{_settingsFileName}\": {ex.Message}");
+            return false;
+        }
+    }
+
+    public static Settings? ReadSettings(IPrinter printer, bool createFileIfMissing = false)
+    {
+        try
+        {
+            if (createFileIfMissing && !CreateIfMissing(printer))
+                return null;
+
+            var text = File.ReadAllText(_settingsFileName);
+            return JsonSerializer.Deserialize<Settings>(text)
+                   ?? throw new JsonException();
+        }
+        catch (FileNotFoundException)
+        {
+            printer.Error($"Settings file \"{_settingsFileName}\" was unexpectedly not found.");
+            return null;
+        }
+        catch (JsonException ex)
+        {
+            printer.Error($"The settings file is invalid: {ex.Message}");
+            return null;
+        }
+        catch (Exception ex)
+        {
+            printer.Error(ex.Message);
+            return null;
+        }
+    }
+
+    public static bool WriteSettingsToFile(Settings settings, IPrinter printer)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(
+                settings,
+                new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(
+                        System.Text.Unicode.UnicodeRanges.All)
+                });
+            File.WriteAllText(_settingsFileName, json);
+            return true;
+        }
+        catch (FileNotFoundException)
+        {
+            printer.Error($"Settings file \"{_settingsFileName}\" is missing.");
+            return false;
+        }
+        catch (JsonException ex)
+        {
+            printer.Error($"The settings file is invalid: {ex.Message}");
+            return false;
+        }
+    }
 }
