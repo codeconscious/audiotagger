90f1703a1f12fa9b00c6b310dd4dc98717be3704
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-01-26 18:44:14 +0900

    Add new unsafe path chars

--- AudioTagger.Library/IOUtilities.cs
+++ AudioTagger.Library/IOUtilities.cs
@@ -11,7 +11,7 @@ public static class IOUtilities
     /// Characters considered invalid for use in file paths.
     /// </summary>
     private static readonly char[] UnsafePathChars =
-        [':', '?', '/', '"', '|'];
+        [':', '?', '/', '⧸', '"', '|', '*'];
 
     public static readonly Func<string, bool> IsSupportedFileExtension =
         new(
