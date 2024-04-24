06aae5905ab5cb636eece2ad48ffbff679e6b88f
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-04-24 09:16:20 +0900

    Add setting to reset artist-genre data (#61)
    * Add and use new setting
    
    * Update todos
    
    * Nomenclature improvements
    
    * Refactor most-populous genre to fix potential bug
    
    * Extract lambdas
    
    * Ignore .deb files
    
    * New private expression-bodied methods
    
    * Cancel genre extraction when there are no applicable files; minor tweaks
    
    * Get existing genre data after checking for relevant media files
    
    * Don't show before and after counts if there was no data before
    
    * Improve before-and-after summary
    
    * Code layout

--- AudioTagger.Console/TagGenreExtractor.cs
+++ AudioTagger.Console/TagGenreExtractor.cs
@@ -4,54 +4,84 @@ namespace AudioTagger.Console;
 
 public sealed class TagGenreExtractor : IPathOperation
 {
-    public void Start(IReadOnlyCollection<MediaFile> mediaFiles,
-                      DirectoryInfo workingDirectory,
-                      Settings settings,
-                      IPrinter printer)
+    private static bool HasGenresAndArtists(MediaFile file) =>
+        file.Genres.Length != 0 &&
+        file.Artists.Length != 0;
+
+    private static string FirstArtistTrimmed(MediaFile file) =>
+        file.Artists[0].Trim();
+
+    private static string ArtistName(IGrouping<string, MediaFile> group) =>
+        group.Key;
+
+    private static string MostPopulousGenre(IGrouping<string, MediaFile> group) =>
+        group
+            .Select(f => f.Genres.First())
+            .GroupBy(genre => genre)
+            .OrderByDescending(group => group.Count())
+            .Select(group => group.Key)
+            .First() // Keep only the most populous genre.
+            .Trim();
+
+    public void Start(
+        IReadOnlyCollection<MediaFile> mediaFiles,
+        DirectoryInfo workingDirectory,
+        Settings settings,
+        IPrinter printer)
     {
         if (string.IsNullOrWhiteSpace(settings.ArtistGenreCsvFilePath))
         {
-            printer.Error("You must specify a .csv containing artist and genre data in your settings file under the 'artistGenreCsvFilePath' key.");
+            printer.Error("You must specify a .csv file path for artist and genre data in your settings file under the 'artistGenreCsvFilePath' key.");
             return;
         }
 
         Watch watch = new();
 
-        Dictionary<string, string> existingGenres;
-        var readResult = GenreService.Read(settings.ArtistGenreCsvFilePath);
-        if (readResult.IsSuccess)
+        var relevantFiles = mediaFiles.Where(HasGenresAndArtists);
+        if (!relevantFiles.Any())
         {
-            existingGenres = readResult.Value;
-            printer.Print($"Found {existingGenres.Count} genres.");
-            printer.Print($"Will update \"{settings.ArtistGenreCsvFilePath}\".");
-        }
-        else
-        {
-            existingGenres = [];
-            printer.Print($"Will create \"{settings.ArtistGenreCsvFilePath}\".");
+            printer.Error("There are no media files with an artist and genre to process.");
+            return;
         }
 
         var latestGenres =
-            mediaFiles
-                .Where(f => f.Genres.Any() && f.Artists.Any())
-                .GroupBy(f =>
-                    f.Artists[0].Trim(), // Only the first artist (though it might be nice to process all someday)
-                    f => f.Genres.GroupBy(g => g)
-                                 .OrderByDescending(grp => grp.Count())
-                                 .Select(grp => grp.Key)
-                                 .First() // Keep only the most populous genre.
-                                 .Trim())
+            relevantFiles
+                .GroupBy(FirstArtistTrimmed)
                 .ToImmutableSortedDictionary(
-                    f => f.Key,
-                    f => f.First()
-                );
+                    ArtistName,
+                    MostPopulousGenre);
 
         printer.Print($"Found {latestGenres.Count:#,##0} unique artists with genres.");
 
-        var mergedGenres = latestGenres
-            .Concat(existingGenres)
-            .GroupBy(e => e.Key)
-            .ToDictionary(g => g.Key, g => g.First().Value); // Prioritize the first dictionary's values.
+        Dictionary<string, string> existingGenres;
+        if (settings.ResetSavedArtistGenres)
+        {
+            printer.Print("Will delete previous genre data, if any.");
+            existingGenres = [];
+        }
+        else
+        {
+            var readResult = GenreService.Read(settings.ArtistGenreCsvFilePath);
+            if (readResult.IsSuccess)
+            {
+                printer.Print($"Will update \"{settings.ArtistGenreCsvFilePath}\".");
+                existingGenres = readResult.Value;
+                printer.Print($"Found previous genre data for {existingGenres.Count} artist(s).");
+            }
+            else
+            {
+                printer.Print($"Will create \"{settings.ArtistGenreCsvFilePath}\".");
+                existingGenres = [];
+            }
+        }
+
+        var mergedGenres =
+            latestGenres
+                .Concat(existingGenres)
+                .GroupBy(g => g.Key)
+                .ToImmutableDictionary(
+                    g =>g.Key,
+                    g => g.First().Value); // Prioritize the first dictionary's values.
 
         WriteSummary(existingGenres.Count, mergedGenres.Count, printer);
 
@@ -65,9 +95,11 @@ public sealed class TagGenreExtractor : IPathOperation
         static void WriteSummary(int beforeCount, int afterCount, IPrinter printer)
         {
             int countDiff = afterCount - beforeCount;
-            var actionTaken = countDiff < 0 ? "removed" : "added";
-            var beforeVsAfter = $"{beforeCount:#,##0} → {afterCount:#,##0}";
-            var diffSummary = $"In total, {Math.Abs(countDiff)} genres to be {actionTaken} ({beforeVsAfter}).";
+            string actionTaken = countDiff < 0 ? "removed" : "added";
+            string beforeVsAfter = beforeCount == 0
+                ? string.Empty
+                : $" ({beforeCount:#,##0} → {afterCount:#,##0})";
+            string diffSummary = $"In total, {Math.Abs(countDiff)} genres to be {actionTaken}{beforeVsAfter}.";
             printer.Print(diffSummary);
         }
     }
