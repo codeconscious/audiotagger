a751f94e87030e9f61465a6c4675aab1543b0bb7
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-05-28 20:59:49 +0900

    Code cleanup

--- AudioTagger.Console/OperationLibrary.cs
+++ AudioTagger.Console/OperationLibrary.cs
@@ -5,85 +5,85 @@ namespace AudioTagger.Console;
 
 internal static class OperationLibrary
 {
-    internal static readonly IReadOnlyList<Operation> Operations = new List<Operation>()
-    {
-        new(
-            ["-v", "--view"],
-            "View tag data.",
-            new TagViewer()
-        ),
-        new(
-            ["-vs", "--view-summary"],
-            "View a tag data summary.",
-            new TagViewerSummary()
-        ),
-        new(
-            ["-u", "--update"],
-            "Update tag data using filename patterns.",
-            new TagUpdater()
-        ),
-        new(
-            ["-u1", "--update-single"],
-            "Update a single tag in multiple files to a single, manually-specified value.",
-            new TagUpdaterSingle()
-        ),
-        new(
-            ["-ug", "--update-genres"],
-            "Update the genres in all files automatically using the artist-genre data in the settings.",
-            new TagUpdaterGenreOnly()
-        ),
-        new(
-            ["-um", "--update-multiple"],
-            "Update a single tag with multiple values for multiple files.",
-            new TagUpdaterMultiple()),
-        new(
-            ["-uy", "--update-year"],
-            "Update the year using media files' own dates of creation. (Must do before other updates, lest the creation date be modified.)",
-            new TagUpdaterYearOnly()
-        ),
-        new(
-            ["-urt", "--reverse-track-numbers"],
-            "Reverse the track numbers of the given files.",
-            new TagUpdaterReverseTrackNumbers()
-        ),
-        new(
-            ["-r", "--rename"],
-            "Rename and reorganize files into folders based on tag data.",
-            new MediaFileRenamer()
-        ),
-        new(
-            ["-d", "--duplicates"],
-            "List tracks with identical artists and titles. No files are modified or deleted.",
-            new TagDuplicateFinder()
-        ),
-        new(
-            ["-s", "--stats"],
-            "Display file statistics based on tag data.",
-            new TagStats()
-        ),
-        // new(
-        //     ["-n", "--normalize", "--replaygain"],
-        //     "Apply track normalization.",
-        //     new Normalization()),
-        new(
-            ["-g", "--genres"],
-            "Save the primary genre for each artist to a genre file.",
-            new TagGenreExtractor()),
-        new(
-            ["-p", "--parse"],
-            "Get a single tag value by parsing the data of another (generally Comments).",
-            new TagParser()),
-        new(
-            ["--scan"],
-            "Ad-hoc maintenance scanning work. (Not intended for normal use.)",
-            new TagScanner(),
-            isHidden: true),
-        new(
-            ["--cache-tags"],
-            "Cache files' tag data locally to a JSON file whose path is specified in the settings. (Eventually, this will be helpful in speeding up certain operations.)",
-            new TagCacher(),
-            isHidden: true),
-    };
+    internal static readonly IReadOnlyList<Operation> Operations =
+        [
+            new(
+                ["-v", "--view"],
+                "View tag data.",
+                new TagViewer()
+            ),
+            new(
+                ["-vs", "--view-summary"],
+                "View a tag data summary.",
+                new TagViewerSummary()
+            ),
+            new(
+                ["-u", "--update"],
+                "Update tag data using filename patterns.",
+                new TagUpdater()
+            ),
+            new(
+                ["-u1", "--update-single"],
+                "Update a single tag in multiple files to a single, manually-specified value.",
+                new TagUpdaterSingle()
+            ),
+            new(
+                ["-ug", "--update-genres"],
+                "Update the genres in all files automatically using the artist-genre data in the settings.",
+                new TagUpdaterGenreOnly()
+            ),
+            new(
+                ["-um", "--update-multiple"],
+                "Update a single tag with multiple values for multiple files.",
+                new TagUpdaterMultiple()),
+            new(
+                ["-uy", "--update-year"],
+                "Update the year using media files' own dates of creation. (Must do before other updates, lest the creation date be modified.)",
+                new TagUpdaterYearOnly()
+            ),
+            new(
+                ["-urt", "--reverse-track-numbers"],
+                "Reverse the track numbers of the given files.",
+                new TagUpdaterReverseTrackNumbers()
+            ),
+            new(
+                ["-r", "--rename"],
+                "Rename and reorganize files into folders based on tag data.",
+                new MediaFileRenamer()
+            ),
+            new(
+                ["-d", "--duplicates"],
+                "List tracks with identical artists and titles. No files are modified or deleted.",
+                new TagDuplicateFinder()
+            ),
+            new(
+                ["-s", "--stats"],
+                "Display file statistics based on tag data.",
+                new TagStats()
+            ),
+            // new(
+            //     ["-n", "--normalize", "--replaygain"],
+            //     "Apply track normalization.",
+            //     new Normalization()),
+            new(
+                ["-g", "--genres"],
+                "Save the primary genre for each artist to a genre file.",
+                new TagGenreExtractor()),
+            new(
+                ["-p", "--parse"],
+                "Get a single tag value by parsing the data of another (generally Comments).",
+                new TagParser()),
+            new(
+                ["--scan"],
+                "Ad-hoc maintenance scanning work. (Not intended for normal use.)",
+                new TagScanner(),
+                isHidden: true),
+            new(
+                ["--cache-tags"],
+                "Cache files' tag data locally to a JSON file whose path is specified in the settings. (Eventually, this will be helpful in speeding up certain operations.)",
+                new TagCacher(),
+                isHidden: true),
+        ];
 
     public static Dictionary<string, string> GenerateHelpTextPairs(bool includeHidden)
     {
@@ -108,7 +108,8 @@ internal static class OperationLibrary
             : Result.Ok(maybeOperation);
     }
 
-    public static Result<ImmutableList<IPathOperation>> GetPathOperations(IEnumerable<string> requestedOperations)
+    public static Result<ImmutableList<IPathOperation>> GetPathOperations(
+        IEnumerable<string> requestedOperations)
     {
         var successes = new List<IPathOperation>();
         var failures = new List<string>();
