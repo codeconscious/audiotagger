1b58e8ad5f81204da020c7374aef43f98bd585dc
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2024-06-16 12:05:47 +0900

    Add xmldoc comments for `Duplicates` class

--- AudioTagger.Library/Settings/Settings.cs
+++ AudioTagger.Library/Settings/Settings.cs
@@ -8,17 +8,17 @@ public sealed record Settings
     {
         return new Settings()
             {
-                Duplicates = new(),
                 Tagging = new(),
+                Duplicates = new(),
                 Renaming = new()
             };
     }
 
-    [JsonPropertyName("duplicates")]
-    public Duplicates Duplicates { get; set; } = new();
-
     [JsonPropertyName("tagging")]
     public Tagging? Tagging { get; set; }
+
+    [JsonPropertyName("duplicates")]
+    public Duplicates Duplicates { get; set; } = new();
 
     [JsonPropertyName("renaming")]
     public Renaming? Renaming { get; set; }
@@ -33,20 +33,51 @@ public sealed record Settings
     public string? TagCacheFilePath { get; set; }
 }
 
+/// <summary>
+/// Settings used for finding audio files with identical or near-identical
+/// tags. It provides the ability to ignore certain facets of tag data
+/// for a more custom search.
+/// </summary>
 public sealed record Duplicates
 {
+    /// <summary>
+    /// An optional collection of strings, each of which, when found,
+    /// should be ignored in the track title for purposes of finding
+    /// duplicates. (Operations are done in memory only, and the actual
+    /// title tag is not modified.)
+    /// </summary>
     [JsonPropertyName("titleReplacements")]
     public ImmutableList<string>? TitleReplacements { get; set; }
 
+    /// <summary>
+    /// Before saving a playlist file, search the full paths of the
+    /// files to be included for the following text (which will be
+    /// replaced).
+    /// </summary>
+    /// <remarks>Used in conjunction with `PathReplaceWith`.</remarks>
     [JsonPropertyName("pathSearchFor")]
     public string? PathSearchFor { get; set; }
 
+    /// <summary>
+    /// Before saving a playlist file, replace the string identified in
+    /// `PathSearchFor` and replace all occurrences with this string,
+    /// if specified.
+    /// </summary>
+    /// <remarks>Used in conjunction with `PathSearchFor`.</remarks>
     [JsonPropertyName("pathReplaceWith")]
     public string? PathReplaceWith { get; set; }
 
+    /// <summary>
+    /// The full directory path to which the playlist of duplicates
+    /// should be saved. (Its filename will be automatically generated.)
+    /// </summary>
     [JsonPropertyName("savePlaylistDirectory")]
     public string? SavePlaylistDirectory { get; set; }
 
+    /// <summary>
+    /// An optional collection of track artists and/or titles that
+    /// should be excluded from duplicate searches.
+    /// </summary>
     [JsonPropertyName("exclusions")]
     public ImmutableList<ExclusionPair>? Exclusions { get; set; }
 }
