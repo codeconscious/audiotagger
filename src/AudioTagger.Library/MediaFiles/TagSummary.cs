18b0d82853b573a10fee25462ef4116f02b2f33c
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-02-02 17:24:54 +0900

    Add TagSummary class

---
+++ src/AudioTagger.Library/MediaFiles/TagSummary.cs
@@ -0,0 +1,22 @@
+namespace AudioTagger.Library.MediaFiles;
+
+public class TagSummary : IMediaData
+{
+    public string Title { get; }
+    public string[] AlbumArtists { get; }
+    public string[] Artists { get; }
+    public string Album { get; }
+    public uint Year { get; }
+    public uint TrackNo { get; }
+    public TimeSpan Duration { get; }
+    public string[] Genres { get; }
+    public string[] Composers { get; }
+    public string Lyrics { get; }
+    public string Comments { get; }
+    public string Description { get; }
+    public int BitRate { get; }
+    public int SampleRate { get; }
+    public double ReplayGainTrack { get; }
+    public double ReplayGainAlbum { get; }
+    public byte[] AlbumArt { get; }
+}
