1c35f5a9c322d3fb55d322ed013545f0ffd24583
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-02-07 16:19:59 +0900

    Add default value to TagSummary members

--- src/AudioTagger.Library/MediaFiles/TagSummary.cs
+++ src/AudioTagger.Library/MediaFiles/TagSummary.cs
@@ -2,21 +2,21 @@ namespace AudioTagger.Library.MediaFiles;
 
 public class TagSummary : IMediaData
 {
-    public string Title { get; }
-    public string[] AlbumArtists { get; }
-    public string[] Artists { get; }
-    public string Album { get; }
+    public string Title { get; } = string.Empty;
+    public string[] AlbumArtists { get; } = [];
+    public string[] Artists { get; } = [];
+    public string Album { get; } = string.Empty;
     public uint Year { get; }
     public uint TrackNo { get; }
     public TimeSpan Duration { get; }
-    public string[] Genres { get; }
-    public string[] Composers { get; }
-    public string Lyrics { get; }
-    public string Comments { get; }
-    public string Description { get; }
+    public string[] Genres { get; } = [];
+    public string[] Composers { get; } = [];
+    public string Lyrics { get; } = string.Empty;
+    public string Comments { get; } = string.Empty;
+    public string Description { get; } = string.Empty;
     public int BitRate { get; }
     public int SampleRate { get; }
     public double ReplayGainTrack { get; }
     public double ReplayGainAlbum { get; }
-    public byte[] AlbumArt { get; }
+    public byte[] AlbumArt { get; } = [];
 }
