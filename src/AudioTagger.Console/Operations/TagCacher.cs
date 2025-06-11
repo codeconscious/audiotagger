e9d7edeab9b6294ecd35fafc9aaaa5b84ebd4d79
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-06-11 18:04:37 +0900

    Save tag data to a local JSON file (#93)
    * Add LastWriteTime to cached data
    
    * Add album artists
    
    * Add IMediaData interface
    
    * Add TagSummary class
    
    * Remove setters from interface
    
    * Add default value to TagSummary members
    
    * Don't unescape JSON (as it causes errors for me in another tool)
    
    * Remove IMediaFile; etc.
    
    * Remove TagSummary record type; add path info to JSON
    
    * Move title to top of the confirm-tag dialog
    
    * Fix text
    
    * Remove comment

--- src/AudioTagger.Console/Operations/TagCacher.cs
+++ src/AudioTagger.Console/Operations/TagCacher.cs
@@ -7,16 +7,6 @@ namespace AudioTagger.Console.Operations;
 
 public sealed class TagCacher : IPathOperation
 {
-    private record TagSummary(
-        string[] Artists,
-        string Album,
-        uint TrackNo,
-        string Title,
-        uint Year,
-        string[] Genres,
-        TimeSpan Duration
-    );
-
     public void Start(
         IReadOnlyCollection<MediaFile> mediaFiles,
         DirectoryInfo workingDirectory,
@@ -25,35 +15,38 @@ public sealed class TagCacher : IPathOperation
     {
         if (settings.TagCacheFilePath is null)
         {
-            printer.Error("You must specify the save file path in the settings file.");
+            printer.Error("You must specify the save file path in the settings.");
             return;
         }
 
         Watch watch = new();
 
-        var summaries = mediaFiles.Select(m => {
-                return new TagSummary(
-                    m.Artists,
-                    m.Album,
-                    m.TrackNo,
-                    m.Title,
-                    m.Year,
-                    m.Genres,
-                    m.Duration
-                );
+        var summaries =
+            mediaFiles.Select(m => new
+            {
+                m.FileNameOnly,
+                m.FileInfo.DirectoryName,
+                m.Artists,
+                m.AlbumArtists,
+                m.Album,
+                m.TrackNo,
+                m.Title,
+                m.Year,
+                m.Genres,
+                m.Duration,
+                m.FileInfo.LastWriteTime,
             });
 
-        printer.Print("Serializing the tags to JSON...");
+        printer.Print("Serializing tag data to JSON...");
         JsonSerializerOptions options = new()
         {
             WriteIndented = true,
             Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
         };
         var json = JsonSerializer.Serialize(summaries, options);
-        var unescapedJson = System.Text.RegularExpressions.Regex.Unescape(json); // Avoids `\0027`, etc.
 
         printer.Print($"Saving cached tag data to \"{settings.TagCacheFilePath}\"...");
-        File.WriteAllText(settings.TagCacheFilePath, unescapedJson);
+        File.WriteAllText(settings.TagCacheFilePath, json);
         printer.Print($"Saved in {watch.ElapsedFriendly}.");
     }
 }
